edaf7a7db885a6f538bdc710622c107d9a15dda2
MrHidalgo <Luckyhidalgo@gmail.com>
2016-11-13 13:32:37 +0200

    Small fix PROFILE page

--- dist/script/profile.js
+++ dist/script/profile.js
@@ -1,13 +1,39 @@
-function addHeightAfterResize() {
-    if($("body").hasClass("hello__page")) {
-        var heightBody = $(document).height();
+$(document).ready( function() {
 
-        $(".hello_bg").css(
-            "height", heightBody
-        );
-    }
-}
+    $(".goals__btn-main").on("click", function(e) {
+        e.preventDefault();
 
-$(window).on("load resize", function() {
-    addHeightAfterResize();
+        if($(this).closest(".goals__btn-item").hasClass("active")) {
+            $(".goals__btn-item, .form__message").removeClass("active");
+            return false;
+        }
+
+        $(".goals__btn-item").removeClass("active");
+
+        $(this).closest(".goals__btn-item").addClass("active");
+        $(".form__message").addClass("active");
+    });
+
+
+
+    var blockGoals = '<div class="goals__wrap"> ' +
+        '<textarea name="form__message" class="form__message form__message_profile"></textarea> ' +
+        '<div class="goals__btn">' +
+            '<ul class="goals__btn-list"> ' +
+                '<li class="goals__btn-item">' +
+                    '<a href="javascript:void(0);" class="goals__btn-main goals__btn-liked">' +
+                        '<i class="icon goals__btn-icon"></i></a></li> ' +
+                '<li class="goals__btn-item">' +
+                    '<a href="javascript:void(0);" class="goals__btn-main goals__btn-edit">' +
+                        '<i class="icon goals__btn-icon"></i></a></li> ' +
+                '<li class="goals__btn-item">' +
+                    '<a href="javascript:void(0);" class="goals__btn-main goals__btn-delete">' +
+                        '<i class="icon goals__btn-icon"></i></a></li></ul></div></div>';
+
+
+    $(".btn-add").on("click", function(e) {
+        e.preventDefault();
+
+        $('.goals').append(blockGoals);
+    });
 });
